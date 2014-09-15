560f73b3686f5e4aecb87119a1f590370c9c172d
Charles Oliveira <18oliveira.charles@gmail.com>
2014-09-15 07:24:57 -0300

    Initiated bikex main loop, just for fun

--- bikex/Bikex.cpp
+++ bikex/Bikex.cpp
@@ -7,8 +7,10 @@
 #include <Ovr.h>
 #include <Unity.h>
 #include <iostream>
+#include <unistd.h>
 
 using namespace std;
+
 
 Bikex::Bikex()
 {
@@ -51,4 +53,61 @@ int Bikex::writeDevices()
 int Bikex::readDevices()
 {
 	return 0;
+}
+
+void Bikex::play()
+{
+	bool keepGoing = true;
+	while(keepGoing)
+	{
+
+		unity;
+		ovr;
+		battery;
+		_break;
+		direction;
+		oximetry;
+		speed;
+
+		calculatePlayerPosition();
+		/*
+		However if you use this fps value directly and just display it to the screen, you'll find it's rather irratic. The amount of time it takes to complete a frame will typically vary a bit from frame to frame, and this will make it hard to read and not so useful. To get around this, you can filter your result a bit taking a bunch of samples and averaging the result.
+
+
+
+		static const int NUM_FPS_SAMPLES = 64;
+
+		float fpsSamples[NUM_FPS_SAMPLES]
+
+		int currentSample = 0;
+
+
+
+		float CalcFPS(int dt)
+
+		{
+
+		    fpsSamples[currentSample % NUM_FPS_SAMPLES] = 1.0f / dt;
+
+		    float fps = 0;
+
+		    for (int i = 0; i < NUM_FPS_SAMPLES; i++)
+
+		        fps += fpsSamples[i];
+
+		    fps /= NUM_FPS_SAMPLES;
+
+		    return fps;
+
+		}
+		*/
+
+		usleep(calcFPS(0));
+	}
+
+}
+
+float Bikex::calcFPS(int dt)
+{
+	return 0.0;
 }
--- bikex/Bikex.h
+++ bikex/Bikex.h
@@ -1,9 +1,58 @@
 #ifndef BIKEX_H
 #define BIKEX_H
 
+#include <Battery.h>
+#include <Oximetry.h>
+#include <Speed.h>
+#include <Direction.h>
+#include <Break.h>
+#include <Unity.h>
+#include <Ovr.h>
+
 class Bikex
 {
 public:
+
+	/**
+	 *	How many frames per second
+	 */
+	int FRAME_RATE;
+
+	/**
+	 *	Our own reference to Unity
+	 */
+	Unity unity;
+
+	/**
+	 *	Our own reference to Ovr
+	 */
+	Ovr ovr;
+
+	/**
+	 *	Our own reference to Battery
+	 */
+	Battery battery;
+
+	/**
+	 *	Our own reference to Break
+	 */
+	Break _break;
+
+	/**
+	 *	Our own reference to Direction
+	 */
+	Direction direction;
+
+	/**
+	 *	Our own reference to Oximetry
+	 */
+	Oximetry oximetry;
+
+	/**
+	 *	Our own reference to Speed
+	 */
+	Speed speed;
+
 	Bikex();
 	~Bikex();
 
@@ -35,6 +84,17 @@ public:
 	 *	Return 0 if success != otherwise
 	 */
 	int readDevices();
+
+	/**
+	 *	Method that is going to be executed 30 times a second
+	 */
+	void play();
+
+	/**
+	 *	Calculate how long to sleep before building next frame
+	 *	it should return time in u_seconds to wait for the next frame to be built
+	 */
+	float calcFPS(int dt);
 };
 
 #endif
