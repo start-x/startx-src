e7d54dcb736b52f1c638a29e853dfda812a26934
Ziul <ziuloliveira@gmail.com>
2014-09-16 11:33:13 -0300

    Added script to configure/install doxygen

---
+++ device/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""@package Sensor
+
+ Universidade de Brasilia - FGA
+
+"""
+
+# License type
+__license__ = "MIT"
+# Date release
+__date__ = '2014'
+# Actual version
+__version__ = "0.1"
