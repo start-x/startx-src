d830b2c31a9725427b6012746a581db678506586
Ziul <ziuloliveira@gmail.com>
2014-09-20 18:54:17 -0300

    Initial signal commit

---
+++ bikex/Wrap.h
@@ -0,0 +1,8 @@
+
+#include <signal.h>
+
+
+#define SIG1 30 //SIGUSR1 
+#define SIG2 31 //SIGUSR2
+#define SIG3 14 //SIGALRM
+#define SIG4 19 //SIGCONT
---
+++ device/scripts/interface.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+"""
+	Interface to contact the MSN430
+"""
+
+
+import startx
+
+print startx.SIG1
+print startx.SIG2
+print startx.SIG3
+print startx.SIG4
