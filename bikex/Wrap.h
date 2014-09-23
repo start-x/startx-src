41616269f59505e2cb12b954d44818ac36f8fb1b
Ziul <ziuloliveira@gmail.com>
2014-09-23 14:30:15 -0300

    Rewrite Makefile to build more informative files

--- bikex/Wrap.h
+++ bikex/Wrap.h
@@ -3,4 +3,5 @@
 #define SIG1 30 //SIGUSR1 
 #define SIG2 31 //SIGUSR2
 #define SIG3 14 //SIGALRM
-#define FILE "/tmp/bikex"
+#define ACTIVE_FILE "/tmp/bikex_active"
+#define PASSIVE_FILE "/tmp/bikex_passive"
