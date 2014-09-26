d05d6908f07f59fbb36520a1259b9be2dc27c9d6
Charles Oliveira <18oliveira.charles@gmail.com>
2014-09-26 16:18:47 -0300

    Little fix for puppet instalation

--- setup_development_environment.sh
+++ setup_development_environment.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-dpkg -l puppet
-if [ $? != 0 ]; then
-  sudo apt-get install puppet
-fi
-
+sudo apt-get install puppet
 sudo puppet apply --modulepath=./puppet/modules/ ./puppet/manifests/site.pp
 
