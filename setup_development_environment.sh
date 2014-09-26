934f83f0e79e7e610e2d17cf56f7548f7c2507cf
Lucas Kanashiro <kanashiro.duarte@gmail.com>
2014-09-26 18:23:25 -0300

    Upgrading puppet module

--- setup_development_environment.sh
+++ setup_development_environment.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sudo apt-get install puppet
+puppet module install --modulepath=./puppet/modules tracywebtech-pip
 sudo puppet apply --modulepath=./puppet/modules/ ./puppet/manifests/site.pp
 
